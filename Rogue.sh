deb832c882ad8c4418e07782a739208891170a87
RAthan_rithik <rithikrathan.c@gmail.com>
2025-03-31 09:25:21 +0530

    After some useless changes it now looks good and now moving on to the templates

--- Rogue.sh
+++ Rogue.sh
@@ -13,9 +13,7 @@ TEMPLATE="default"
 # Parse arguments
 while [[ $# -gt 0 ]]; do
 	case "$1" in
-		new)echo "-------------------------------------------------------------------"
-			read -p "[Rogue] Enter project name: " PROJECT_NAME
-			echo "-------------------------------------------------------------------"
+		new)read -p $'\e[1m[Rogue]\e[0m Enter project name: ' PROJECT_NAME
 			shift 
 			;;
 			
@@ -23,7 +21,8 @@ while [[ $# -gt 0 ]]; do
 			
 		-v) 
 			if [[ "$2" != "public" && "$2" != "private" ]]; then
-				echo "[Rogue] Error: Visibility must be 'public' or 'private'."
+				echo -e "\e[1m[Rogue]\e[0m Error: Visibility must be 'public' or 'private'."
+				echo -e "\e[1m[Rogue]\e[0m Exiting......"
 				return 1
 			fi
 			REPO_VISIBILITY="$2";
@@ -31,21 +30,23 @@ while [[ $# -gt 0 ]]; do
 			;;
 			
 		-t) if [[ -z "$2" ]];then
-				echo "[Rogue] Error: Template name cannot be empty."
+				echo -e "\e[1m[Rogue]\e[0m Error: Template name cannot be empty."
+				echo -e "\e[1m[Rogue]\e[0m Exiting......"
 				return 1
 			fi
 			
 			if [[ -d "$TEMPLATE_DIR/$2" ]];then
 				TEMPLATE="$2"; 
-				shift 2 
 			else
-				echo "[Rogue] Error: Template does not exist in the template directory using default template"
-				TEMPLATE="$2"
+				echo -e "\e[1m[Rogue]\e[0m Error: Template does not exist in the template directory using default template"
+				TEMPLATE="default"
 			fi
+			shift 2 
 			;;
 			
 		-m) if [[ -z "$2" ]];then
-				echo "[Rogue] Error: Commit message cannot be empty."
+				echo -e "\e[1m[Rogue]\e[0m Error: Commit message cannot be empty."
+				echo -e "\e[1m[Rogue]\e[0m Exiting......"
 				return 1
 			fi
 			COMMIT_MSG="$2"; 
@@ -59,52 +60,51 @@ PROJECT_DIR="$PROJECTS_DIR$PROJECT_NAME"
 # echo "projectDirectory: $PROJECT_DIR"
 
 # Create project directory:
-echo "\n [~~~~~~~~~~~~~~~~~~|Creating project directory|~~~~~~~~~~~~~~~~]"
+echo -e "\n[~~~~~~~~~~~~~~~~~~|Creating project directory|~~~~~~~~~~~~~~~~]"
 # Creates a project directory {thats how your comment should be}
 if [ -e "$PROJECT_DIR" ];then
 	if [ "$REPLACE" == "true" ];then
-		echo "[Rogue] Replacing existing directory....."
+		echo -e "\e[1m[Rogue]\e[0m Replacing existing directory....."
 		rm -rf "$PROJECT_DIR"
 		mkdir -p "$PROJECT_DIR"
-		echo "[Rogue] Current directory is set to $PROJECT_DIR"
+		echo -e "\e[1m[Rogue]\e[0m Current directory is set to $PROJECT_DIR"
 		cd "$PROJECT_DIR" 
 	else
-		echo "[Rogue] Error: Directory already exists! Try again with -r flag to replace it (will delete existing data)"
-		echo "[Rogue] Exiting......"
+		echo -e "\e[1m[Rogue]\e[0m Error: Directory already exists! Try again with -r flag to replace it (will delete existing data)"
+		echo -e "\e[1m[Rogue]\e[0m Exiting......"
 		return 1
 	fi
 else
-	echo "[Rogue] Creating project directory....."
+	echo -e "\e[1m[Rogue]\e[0m Creating project directory....."
 	mkdir -p "$PROJECT_DIR"
-	echo "[Rogue] Current directory is set to $PROJECT_DIR"
+	echo -e "\e[1m[Rogue]\e[0m Current directory is set to $PROJECT_DIR"
 	cd "$PROJECT_DIR"
 fi
 
-echo "\n [~~~~~~~~~~~~~~~~~|Creating GitHub repository|~~~~~~~~~~~~~~~~~]"
+echo -e "\n[~~~~~~~~~~~~~~~~~|Creating GitHub repository|~~~~~~~~~~~~~~~~~]"
 # Check if the GitHub CLI is authenticated
 if ! gh auth status &>/dev/null; then
-	echo "[Rogue] Error: GitHub CLI is not authenticated or timed out. try running  'gh auth login' first or checking your internet connection."
-	echo "[Rogue] Exiting....."
+	echo -e "\e[1m[Rogue]\e[0m Error: GitHub CLI is not authenticated or timed out. try running  'gh auth login' first or checking your internet connection."
+	echo -e "\e[1m[Rogue]\e[0m Exiting....."
 	return 1
 fi
 # Get GitHub username using GitHub API
 GITHUB_USER=$(gh api user --jq .login)
-echo "-------------------------------------------------------------------"
-echo "[Rogue] GitHub username: $GITHUB_USER"
-echo "-------------------------------------------------------------------"
+echo -e "\e[1m[Rogue]\e[0m GitHub username: $GITHUB_USER \n"
+
 # Initialize Git
 git init
 gh repo create "$PROJECT_NAME" --"$REPO_VISIBILITY" --source=. --remote=origin
-echo "[Rogue] GitHub repository created: https://github.com/$GITHUB_USER/$PROJECT_NAME"
+echo -e "\e[1m[Rogue]\e[0m GitHub repository created: https://github.com/$GITHUB_USER/$PROJECT_NAME"
 
-echo "\n [~~~~~~~~~~~~~~~|Creating Basic files and folders|~~~~~~~~~~~~~]"
-echo "[Rogue] Using $TEMPLATE template......"
+echo -e "\n[~~~~~~~~~~~~~~~|Creating Basic files and folders|~~~~~~~~~~~~~]"
+echo -e "\e[1m[Rogue]\e[0m Using $TEMPLATE template......"
 ~/Desktop/projects/RoguePM/RogueTemplates/$TEMPLATE/$TEMPLATE.sh -n "$PROJECT_NAME"
 
-echo "\n [~~~~~~~~~~~~~~~~~~~~|Making initial commit|~~~~~~~~~~~~~~~~~~~]"
+
+echo -e "\n[~~~~~~~~~~~~~~~~~~~~|Making initial commit|~~~~~~~~~~~~~~~~~~~]"
 git add .
 git commit -m "$COMMIT_MSG"
-echo "\n [~~~~~~~~~~~~~~~~~~~~|Pushing to main branch|~~~~~~~~~~~~~~~~~~]"
+echo -e "\n[~~~~~~~~~~~~~~~~~~~~|Pushing to main branch|~~~~~~~~~~~~~~~~~~]"
 git push -u origin main
-
-
+echo -e "\n\e[1m[Rogue]\e[0m Exiting......"
