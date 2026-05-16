5a3b451907a7e842c0d1a93c30fe2028fbd7ccf2
Rathan <rithikrathan.c@gmail.com>
2026-05-16 07:58:33 +0400

    rename new username

--- session.sh
+++ session.sh
@@ -5,11 +5,11 @@ SESSION="RoguePM"
 tmux kill-session -t $SESSION 2>/dev/null
 
 # editor window (shell first, then start nvim)
-tmux new-session -d -s $SESSION -n "editor" -c "/home/rathan/Desktop/projects/RoguePM"
+tmux new-session -d -s $SESSION -n "editor" -c "/home/rithik/Desktop/projects/RoguePM"
 tmux send-keys -t $SESSION:editor "nvim" C-m
 
 # shell window
-tmux new-window -t $SESSION -n "shell" -c "/home/rathan/Desktop/projects/RoguePM"
+tmux new-window -t $SESSION -n "shell" -c "/home/rithik/Desktop/projects/RoguePM"
 
 tmux select-window -t $SESSION:editor
 tmux attach-session -t $SESSION
