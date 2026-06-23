b9077e568d58567ad81b88e2b916a8166cc756b0
Rathan <rithikrathan.c@gmail.com>
2026-06-23 13:30:20 +0530

    Addition: logic to remove old socket file form Unix Sockets
    Unix Sockets are the way we gonna communicate with the daemon and it
    will clear all the existing files on each time it boots up

---
+++ TODO.md
@@ -0,0 +1,1 @@
+[] make sure if a venv is present in a project dir to use that env, like when using session.sh
--- rogued/src/main.rs
+++ rogued/src/main.rs
@@ -1,3 +1,39 @@
+// INFO:
+// -----------------------------------------------------------------------------
+// Script: main.rs
+// Version: 0.0.1
+// Author: RITHIK RATHAN C. <github.com/rithikrathan>
+// License:
+// Repository: https://github.com/rithikrathan/RoguePM
+// Project: RoguePM
+// Created: 2026-06-23
+// Description: Very first iteration of the rogued consept of a daemon that handles
+//             distributed project management across a LAN using sshfs and some TCP
+//             connection, idk  how it turns out
+// -----------------------------------------------------------------------------
+
+use core::result::Result::Ok;
+use std::io::ErrorKind;
+// use serde::{Deserialize, Serialize};
+// use std::error::Error; //for errors
+// use tokio::io::{AsyncReadExt, AsyncWriteExt}; // read and write from stream
+// use tokio::new::{UnixListener, UnixStream}; // from unix socket
+
+static SOCKET_PATH: &str = "/tmp/rogued.sock";
+
 fn main() {
     println!("Hello Idiots!");
+    match std::fs::remove_file(SOCKET_PATH) {
+        Ok(()) => {
+            println!("Discarding existing socket files");
+        }
+
+        Err(e) => {
+            if e.kind() == ErrorKind::NotFound {
+                println!("No existing socket files found");
+            } else {
+                println!("Owned by skill issue, \r\nError: {}", e);
+            }
+        }
+    }
 }
