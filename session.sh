084d1589b2c04df3d2ff3a1b8925c50a64f566a5
Rathan <rithikrathan.c@gmail.com>
2026-06-29 12:48:02 +0530

    update: session.sh

--- session.sh
+++ session.sh
@@ -3,6 +3,7 @@
 SESSION="RoguePM"
 BASE_DIR="$(cd "$(dirname "$0")" && pwd)"
 ROGUED_DIR="$BASE_DIR/rogued"
+ROGUED_DIR1="$BASE_DIR/rogued"
 
 tmux kill-session -t "$SESSION" 2>/dev/null
 
@@ -13,7 +14,7 @@ tmux send-keys -t "$SESSION:editor" "nvim" C-m
 # shell window
 tmux new-window -t "$SESSION" -n "shell" -c "$ROGUED_DIR"
 tmux send-keys -t "$SESSION:shell" "clear" C-m
-tmux split-window -h -t "$SESSION:shell" -p 50 -c "$ROGUED_DIR"
+tmux split-window -h -t "$SESSION:shell" -p 50 -c "$ROGUED_DIR1"
 tmux send-keys -t "$SESSION:shell.2" "clear" C-m
 
 # lazygit window
@@ -28,10 +29,10 @@ tmux send-keys -t "$SESSION:superfile" "spf" C-m
 # tmux send-keys -t "$SESSION:docs" "rdocs rust" C-m
 
 # opencode window (opencode left 69%, terminal right 31%)
-# tmux new-window -t "$SESSION" -n "opencode" -c "$BASE_DIR"
-# tmux send-keys -t "$SESSION:opencode" "opencode" C-m
-# tmux split-window -h -t "$SESSION:opencode" -p 31 -c "$ROGUED_DIR"
-# tmux send-keys -t "$SESSION:opencode.2" "clear" C-m
+tmux new-window -t "$SESSION" -n "opencode" -c "$BASE_DIR"
+tmux send-keys -t "$SESSION:opencode" "opencode" C-m
+tmux split-window -h -t "$SESSION:opencode" -p 31 -c "$ROGUED_DIR"
+tmux send-keys -t "$SESSION:opencode.2" "clear" C-m
 
 tmux select-window -t "$SESSION:editor"
 tmux attach-session -t "$SESSION"
